558966881234f6273316ce5e04bd6b325afe9510
samrcell91 <smarcell91@gmail.com>
2021-04-20 14:31:15 +0200

    Player class changed, Game class basics

--- src/java/com/codecool/battleship/Player.java
+++ src/java/com/codecool/battleship/Player.java
@@ -1,37 +1,73 @@
 package java.com.codecool.battleship;
 import java.com.codecool.battleship.board.Ship;
 import java.com.codecool.battleship.board.Square;
+import java.com.codecool.battleship.board.SquareStatus;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Player class has Square[][] board, String name, List<Ship> ships.
+ */
 public class Player {
+    public Square[][] board;
+    public final String name;
     List<Ship> ships = new ArrayList<Ship>();
 
-    public void shoot(Square[][] ocean) {  //get this shoot method the board or the list of ships from the other player??
-        //input from here or from input class??
-        int inputX = 2;
-        int inputY = 4;
+    public Player(String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * Player shoots, and change the shooted Square status.
+     */
+    public void shoot(Square[][] ocean, List<Ship> enemyShips) {
+        Scanner shooting = new Scanner(System.in);
+        while (true) {
+            System.out.println("Give the coordinate:");
+            String coordinate = shooting.nextLine();
+            String XCoordinate = coordinate.substring(0,1);
+            String YCoordinate = coordinate.substring(1);
+            if (validInput(firstCoordinate, secondCoordinate)) break;       // Input validator!!!
+        }
+        // Maybe doesn't need loop
         for (Square[] row: ocean) {
             for (Square cell: row) {
-                if (cell.getX() == inputX && cell.getY() == inputY) {
-                    if (cell.squareStatus == "empty") {
+                if (cell.getX() == XCoordinate && cell.getY() == YCoordinate) {
+                    if (cell.squareStatus == SquareStatus.EMPTY) {              // EMPTY ('.')
                         System.out.println("It's an empty square");
-                        cell.squareStatus = "miss";
+                        cell.SetSquareStatus('X');                              // MISS ('X')
                     }
-                    else if (cell.SquareStatus == "ship") {
+                    else if (cell.SquareStatus == SquareStatus.SHIP) {          // SHIP ('O')
                         System.out.println("It's a HIT");
-                        cell.squareStatus = "hit";
+                        cell.SetSquareStatus('#');                              // HIT ('#');
+                        for (Ship ship: enemyShips) {
+                            for (Square shipCell: ship.tiles) {
+                                if (shipCell.getX() == XCoordinate && shipCell.getY() == YCoordinate) {
+                                    shipCell.SetSquareStatus('#');
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
 
+    /**
+     * Return List<Ship> ships.
+     */
+    public List<Ship> getShips() { return ships; }
+
+    /**
+     * Check that player still have at least one Square ship.
+     */
     public boolean isAlive() {
         for (Ship ship: ships) {
-            for (Square cell: ship.cells) {  //need List name
-                if (cell.squareStatus == "ship") return true;
+            for (Square cell: ship.tiles) {
+                if (cell.GetSquareStatus() == 'O') return true;
             }
         }
         return false;
