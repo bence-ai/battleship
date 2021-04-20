41f60cb017da503140f00aa1acc13030f42bcd82
Plantelo <trabantkedvelo@gmail.com>
2021-04-20 12:02:11 +0200

    Create ship type enum

--- src/java/com/codecool/battleship/board/ShipType.java
+++ src/java/com/codecool/battleship/board/ShipType.java
@@ -1,4 +1,19 @@
 package java.com.codecool.battleship.board;
 
-public class ShipType {
+/**
+ * Enum that defines the types of ships
+ * and their lengths.
+ */
+public enum ShipType {
+    CARRIER (1),
+    CRUISER (2),
+    BATTLESHIP (3),
+    SUBMARINE (4),
+    DESTROYER (5);
+
+    ShipType(int length) { this.length = length; }
+
+    // Number of Square objects to set to "ship" status when a ship of this type is placed
+    private int length;
+    protected int GetLength() { return length; }
 }
