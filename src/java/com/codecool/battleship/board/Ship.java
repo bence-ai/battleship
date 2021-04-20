9a3d95d06f023f41ab35b672cbbc00292dbe3927
Plantelo <trabantkedvelo@gmail.com>
2021-04-20 12:50:48 +0200

    Create data structure for Ship class

--- src/java/com/codecool/battleship/board/Ship.java
+++ src/java/com/codecool/battleship/board/Ship.java
@@ -1,4 +1,26 @@
 package java.com.codecool.battleship.board;
 
+import java.util.List;
+
+/**
+ * Class that represents one ship the
+ * player has to shoot down from their
+ * enemy's board.
+ */
 public class Ship {
+    // attributes
+    private List<Square> tiles;
+
+    // constructor
+
+    /**
+     * Ship object type constructor.
+     * @param startX: Leftmost X coordinate of ship
+     * @param startY: Topmost Y coordinate of ship
+     * @param endX: Rightmost X coordinate of ship
+     * @param endY: Bottom Y coordinate of ship
+     */
+    public Ship(int startX, int startY, int endX, int endY) {
+        // TODO: finish Ship constructor
+    }
 }
