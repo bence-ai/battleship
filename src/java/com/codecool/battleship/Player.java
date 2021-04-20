5c7db543a9d1fe730daf0f1fa24baac2fe4ae906
hubbabubb <gergohuber@gmail.com>
2021-04-20 15:31:30 +0200

    player class constructor create a board

--- src/java/com/codecool/battleship/Player.java
+++ src/java/com/codecool/battleship/Player.java
@@ -1,7 +1,14 @@
 package java.com.codecool.battleship;
 
+import java.com.codecool.battleship.board.Board;
+
 public class Player {
     private String name;
+    private Board board;
+
+    public Player(int size) {
+        this.board = new Board(size);
+    }
 
     public String getName() {
         return name;
